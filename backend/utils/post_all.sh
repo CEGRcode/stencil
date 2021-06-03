6e142ac0322a647cc416e9a2441a362cc94a9518
WilliamKMLai <kai.ming.lai@gmail.com>
2021-06-03 09:13:46 -0400

    Add ATAC-seq example
    Add ATAC-seq example and remove clutter

--- backend/utils/post_all.sh
+++ backend/utils/post_all.sh
@@ -1,16 +1,17 @@
-# PCRP data
-python3 postLibrary.py ../sampleData/GRHL2-17380.json
-python3 postLibrary.py ../sampleData/USF1-26446.json
-python3 postLibrary.py ../sampleData/HNF4A-18755.json
-
-# YEP data
-python3 postLibrary.py ../sampleData/YEP-UMAP.json
-python3 postLibrary.py ../sampleData/Reb1-12141.json
-python3 postLibrary.py ../sampleData/Abf1-19442.json
-python3 postLibrary.py ../sampleData/Rap1-11765.json
-python3 postLibrary.py ../sampleData/Sua7-20022.json
-
-# ENCODE data
-python3 postLibrary.py ../sampleData/CTCF-ENCSR000EGM.json
-python3 postLibrary.py ../sampleData/p300-ENCSR000EGE.json
-python3 postLibrary.py ../sampleData/RNA-seq_DESeq2.json
+## PCRP data
+#python3 postLibrary.py ../sampleData/GRHL2-17380.json
+#python3 postLibrary.py ../sampleData/USF1-26446.json
+#python3 postLibrary.py ../sampleData/HNF4A-18755.json
+#
+## YEP data
+#python3 postLibrary.py ../sampleData/YEP-UMAP.json
+#python3 postLibrary.py ../sampleData/Reb1-12141.json
+#python3 postLibrary.py ../sampleData/Abf1-19442.json
+#python3 postLibrary.py ../sampleData/Rap1-11765.json
+#python3 postLibrary.py ../sampleData/Sua7-20022.json
+#
+## ENCODE data
+#python3 postLibrary.py ../sampleData/CTCF-ENCSR000EGM.json
+#python3 postLibrary.py ../sampleData/p300-ENCSR000EGE.json
+#ython3 postLibrary.py ../sampleData/RNA-seq_DESeq2.json
+python3 postLibrary.py ../sampleData/ATAC-seq_DESeq2.json
